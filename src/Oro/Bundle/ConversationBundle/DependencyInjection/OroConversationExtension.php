c35c295abaa82867b40b34da3f01b9bb98993a8d
Vova Soroka <vsoroka@oroinc.com>
2025-02-22 17:22:35 +0200

    BAP-22934: Disabled entities by feature toggle breaks the API (#40595)

--- src/Oro/Bundle/ConversationBundle/DependencyInjection/OroConversationExtension.php
+++ src/Oro/Bundle/ConversationBundle/DependencyInjection/OroConversationExtension.php
@@ -5,12 +5,10 @@ namespace Oro\Bundle\ConversationBundle\DependencyInjection;
 use Oro\Bundle\ConfigBundle\DependencyInjection\SettingsBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
 use Symfony\Component\DependencyInjection\Loader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\Yaml\Yaml;
 
-class OroConversationExtension extends Extension implements PrependExtensionInterface
+class OroConversationExtension extends Extension
 {
     #[\Override]
     public function load(array $configs, ContainerBuilder $container): void
@@ -26,18 +24,6 @@ class OroConversationExtension extends Extension implements PrependExtensionInte
 
         if ('test' === $container->getParameter('kernel.environment')) {
             $loader->load('services_test.yml');
-        }
-    }
-
-    #[\Override]
-    public function prepend(ContainerBuilder $container): void
-    {
-        if ('test' === $container->getParameter('kernel.environment')) {
-            $fileLocator = new FileLocator(__DIR__ . '/../Tests/Functional/Environment');
-            $configData = Yaml::parse(file_get_contents($fileLocator->locate('app.yml')));
-            foreach ($configData as $name => $config) {
-                $container->prependExtensionConfig($name, $config);
-            }
         }
     }
 }
--- src/Oro/Bundle/ConversationBundle/Tests/Functional/Api/RestJsonApi/ConversationMessagesTest.php
+++ src/Oro/Bundle/ConversationBundle/Tests/Functional/Api/RestJsonApi/ConversationMessagesTest.php
@@ -3,17 +3,35 @@
 namespace Oro\Bundle\ConversationBundle\Tests\Functional\Api\RestJsonApi;
 
 use Oro\Bundle\ApiBundle\Tests\Functional\RestJsonApiTestCase;
+use Oro\Bundle\ConfigBundle\Tests\Functional\Traits\ConfigManagerAwareTestTrait;
 use Oro\Bundle\ConversationBundle\Entity\ConversationMessage;
 use Oro\Bundle\ConversationBundle\Tests\Functional\Api\DataFixtures\LoadMessages;
 use Symfony\Component\HttpFoundation\Response;
 
 class ConversationMessagesTest extends RestJsonApiTestCase
 {
+    use ConfigManagerAwareTestTrait;
+
+    #[\Override]
     protected function setUp(): void
     {
         parent::setUp();
-
         $this->loadFixtures([LoadMessages::class]);
+        $this->setEnableConversationFeature(true);
+    }
+
+    #[\Override]
+    protected function tearDown(): void
+    {
+        $this->setEnableConversationFeature(false);
+        parent::tearDown();
+    }
+
+    private function setEnableConversationFeature(bool $value): void
+    {
+        $configManager = self::getConfigManager();
+        $configManager->set('oro_conversation.enable_conversation', $value);
+        $configManager->flush();
     }
 
     public function testGetList(): void
