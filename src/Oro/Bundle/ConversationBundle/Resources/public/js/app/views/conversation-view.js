2a29def9f5a00ddc9876508f9c7358f979377279
Viacheslav Chekanovskiy <vchekanovskyi@oroinc.com>
2025-05-05 08:30:16 -0600

    BB-25526: Impossible to open conversations list on table/mobile as it immediately switches to first conversation (#41162)

--- src/Oro/Bundle/ConversationBundle/Resources/public/js/app/views/conversation-view.js
+++ src/Oro/Bundle/ConversationBundle/Resources/public/js/app/views/conversation-view.js
@@ -2,10 +2,13 @@ import $ from 'jquery';
 import BaseView from 'oroui/js/app/views/base/view';
 import ConversationModel from '../models/conversation-model';
 import ConversationListView from '../conversation/list/conversation-list-view';
+import ConversationListCollection from '../conversation/list/conversation-list-collection';
 import ConversationMessagesView from '../conversation/messages/conversation-messages-view';
 import ConversationHeaderView from './conversation-header-view';
 import LoadingMaskView from 'oroui/js/app/views/loading-mask-view';
 import template from 'tpl-loader!oroconversation/templates/conversation-view.html';
+import {getSelectedFromLocation} from '../utils/location-util';
+import viewportManager from 'oroui/js/viewport-manager';
 
 const ConversationView = BaseView.extend({
     autoRender: true,
@@ -30,24 +33,29 @@ const ConversationView = BaseView.extend({
             title
         });
 
+        const collection = new ConversationListCollection([], {
+            conversation: this.model
+        });
+
+        !this.model.get('selected') && this.model.set('selected', collection._prepareModel());
+
+        this.subview('loadingMask', new LoadingMaskView());
+
         this.subview('conversationHeader', new ConversationHeaderView({
             model: this.model,
             title: this.title
         }));
 
         this.subview('conversationList', new ConversationListView({
-            model: this.model
+            model: this.model,
+            collection
         }));
 
         this.subview('conversationMain', new ConversationMessagesView({
             model: this.model
         }));
 
-        this.subview('loadingMask', new LoadingMaskView());
-        this.listenToOnce(this.model, 'synced', () => {
-            this.subview('loadingMask').dispose();
-            this.removeSubview('loadingMask');
-        });
+        this.listenToOnce(this.model, 'synced', this.onInitSync);
 
         ConversationView.__super__.initialize.call(this, options);
     },
@@ -101,6 +109,17 @@ const ConversationView = BaseView.extend({
             this.subview('conversationList').selectFirst();
         } else {
             this.onActiveConversation(this.model.get('id'));
+        }
+    },
+
+    onInitSync() {
+        const selectedConversationId = getSelectedFromLocation();
+
+        this.subview('loadingMask').dispose();
+        this.removeSubview('loadingMask');
+
+        if (!viewportManager.isApplicable('tablet-small') || selectedConversationId) {
+            this.subview('conversationList').selectConversation(selectedConversationId);
         }
     }
 });
